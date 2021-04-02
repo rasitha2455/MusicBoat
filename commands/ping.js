48baa656d471da0bcd88aae5d5fd186d4089c71e
rasitha2455 <81703975+rasitha2455@users.noreply.github.com>
2021-04-02 20:41:50 +0530

    Delete commands directory

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    help: {
-        name: "ping",
-        aliases: ["pong"]
-    },
-    run: async (client, message, args) => {
-        return message.channel.send(`🏓 | Pong! \`${Math.round(client.ws.ping)}ms\``);
-    }
-};
